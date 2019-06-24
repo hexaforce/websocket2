bb2713ade96e532a4b2297250526bd2b275d2b32
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-06-24 23:27:12 +0900

    test

--- socket.go
+++ socket.go
@@ -8,16 +8,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:    4096,
-		WriteBufferSize:   4096,
-		EnableCompression: true,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // open open socket.
 func (s *Socket) open() {
